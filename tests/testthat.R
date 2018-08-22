4f29d45c6bfbd11f2ac0f8d832e49dab72d8624d
Georgi Boshnakov <georgi.boshnakov@manchester.ac.uk>
2018-08-22 18:39:45 +0100

    remove/return some tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,4 @@ library(testthat)
 library(sarima)
 
 ## splitting for TravisCI with valgrind:
-test_check("sarima", filter = "^[aAf]")
-test_check("sarima", filter = "^sarima_")
-test_check("sarima", filter = "^sarima$")
-test_check("sarima", filter = "^sarima-")
-
+test_check("sarima")
